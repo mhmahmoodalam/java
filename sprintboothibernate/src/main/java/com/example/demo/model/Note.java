359a12dab4fa730a6bd8c427203e31e6cfa34d02
Muhammed Mahmood Alam <mh.mahmood.alam@gmail.com>
2018-03-13 22:48:58 +0530

    spring boot with hibernate

---
+++ sprintboothibernate/src/main/java/com/example/demo/model/Note.java
@@ -0,0 +1,66 @@
+package com.example.demo.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+
+import java.io.Serializable;
+import java.util.Date;
+
+@Entity
+@Table(name = "notes")
+@EntityListeners(AuditingEntityListener.class)
+@JsonIgnoreProperties(value = { "createdAt", "updatedAt" }, allowGetters = true)
+public class Note implements Serializable {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@Column(name = "ID")
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "id_Sequence")
+	@SequenceGenerator(name = "id_Sequence", sequenceName = "ID_SEQ")
+	private Long id;
+
+	@NotBlank
+	private String title;
+
+	@NotBlank
+	private String content;
+
+	@Column(nullable = false, updatable = false)
+	@Temporal(TemporalType.TIMESTAMP)
+	@CreatedDate
+	private Date createdAt;
+
+	@Column(nullable = false)
+	@Temporal(TemporalType.TIMESTAMP)
+	@LastModifiedDate
+	private Date updatedAt;
+
+	public String getTitle() {
+		// TODO Auto-generated method stub
+		return this.title;
+	}
+
+	public String getContent() {
+		// TODO Auto-generated method stub
+		return this.content;
+	}
+
+	public void setTitle(String title) {
+		// TODO Auto-generated method stub
+		this.title=title;
+	}
+	
+	public void setContent(String content) {
+		// TODO Auto-generated method stub
+		this.content=content;
+	}
+
+	// Getters and Setters ... (Omitted for brevity)
+}
